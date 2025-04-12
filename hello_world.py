c888c3d85360147b8054d3450e43120f910b8a1f
rikyaulia2 <Lambardiy@protonmail.com>
2025-04-12 07:56:49 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
-def main():
+import time
+
+def hello():
     message = "Hello, World!"
     print(message)
-    
-    with open("output.txt", "w") as file:
-        file.write(message)
-    
+    time.sleep(1)  # Pause for 1 second
+    return message
+
 if __name__ == "__main__":
-    main()
+    hello()
